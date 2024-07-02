2c8794fb84f7ca105b3ec8a8193c7d9869990368
Diana <bigdatawithdiana@gmail.com>
2024-07-02 17:45:44 +0100

    Update summary.survcompare.R

--- R/summary.survcompare.R
+++ R/summary.survcompare.R
@@ -9,20 +9,30 @@ summary.survcompare <-
     # prints summary statement for the output of the 'survcompare' function
 
     #check
-    if (!inherits(object, "survcompare")) {
-      stop("Not a \"survcompare\" object")
-    }
+    if (!inherits(object, "survcompare")) {stop("Not a \"survcompare\" object")}
 
     # Cox model Lasso or PH
     coxend <- ifelse(object$useCoxLasso, "CoxLasso  ", "CoxPH    ")
+
+    # SRF or DeepHit
+    if(object$model_name == "DeepHit"){
+      mlmodel = "DeepHit"
+    } else {mlmodel = "Survival Random Forest"}
+
+    # Summary title to print
     x <- object
-    cat(
-      "\nInternally validated test performance of",
-      coxend,
-      "and Survival Random Forest ensemble:\n"
-    )
-    # printing the table with mean test results, diff to CoxPH
-    print(round(x$results_mean, 4))
+    cat("\nInternally validated test performance of",coxend, "and" , mlmodel, ". Mean performance:\n")
+
+    # PRINT MEAN TEST RESULTS
+    printing_columns= c("T", "C_score", "AUCROC","Calib_slope","sec")
+    xx = x$results_mean[printing_columns]
+    print(round(x$results_mean[printing_columns], 4))
+
+    cat("\nMedian performance:\n")
+    # MEDIAN RESULTS
+    printing_columns= c("T", "C_score", "AUCROC","Calib_slope","sec")
+    print(round(x$results_median[printing_columns], 4))
+
     cat("\n")
     pv <- x$difftest["pvalue", "C_score"]
     m <- x$difftest["m", "C_score"]
@@ -33,72 +43,47 @@ summary.survcompare <-
     # compile the output message:
     if (x$difftest["pvalue", "C_score"] < 0.05) {
       t1 <- paste(
-        "Survival Random Forest ensemble has outperformed ",
-        coxend,
-        "by ",
-        round(m, 4),
-        " in C-index.\n",
-        sep = ""
-      )
+        mlmodel, " ensemble has outperformed ",
+        coxend, "by ", round(m, 4)," in C-index.\n",sep = ""    )
       t2 <-
         "The difference is statistically significant with the p-value "
       t3 <-
-        ".\nThe supplied data may contain non-linear or cross-term dependencies, \nbetter captured by the Survival Random Forest.\n"
+        paste(".\nThe supplied data may contain non-linear or cross-term dependencies, \nbetter captured by the",
+              mlmodel, ".\n", sep="")
     } else{
       t1 <- paste(
-        "Survival Random Forest ensemble has NOT outperformed ",
-        coxend,
-        "with mean c-index difference of",
-        round(m, 4),
-        ".\n",
-        sep = ""
-      )
+        mlmodel, " ensemble has NOT outperformed ",
+        coxend, "with mean c-index difference of",
+        round(m, 4), ".\n",sep = "")
       t2 <-
         "The difference is not statistically significant with the p-value = "
       t3 <-
-        ". \nThe data may NOT contain considerable non-linear or cross-term dependencies, \nthat could be captured by the Survival Random Forest.\n"
+        paste(". \nThe data may NOT contain considerable non-linear or cross-term dependencies,\nthat could be captured by",
+              mlmodel, ".\n", sep="")
     }
-
     # print the output message
     cat(paste(t1, t2, ifelse(pv < 0.001, round(pv, 8), round(pv, 4)), pvstars, t3, sep = ""))
 
     #print the main stats
-    ms <- x$main_stats
+    ms <- x$main_stats_pooled
     f <- function(i) {
-      paste(
-        round(mean(ms[i, "mean"]), 4),
-        "(95CI=",
-        round(mean(ms[i, "95CILow"]), 4),
-        "-",
-        round(mean(ms[i, "95CIHigh"]), 4),
-        ";SD=",
-        round(mean(ms[i, "sd"]), 4),
-        ")",
-        sep = ""
-      )
-    }
+      paste(round(mean(ms[i, "mean"]), 4),
+            "(95CI=",round(mean(ms[i, "95CILow"]), 4),"-",
+            round(mean(ms[i, "95CIHigh"]), 4),";SD=",
+            round(mean(ms[i, "sd"]), 4),")",sep = "")}
+    #' to check which row is for AUCROC for Cox and ML,
+    #' if baseline ML was also reported, these are rows 4 and 5
+    #' if not, then rows 3 and 4, so we add 1 if needed to 3 and 4
+    ii = ifelse(dim(ms)[1]==4, 0, 1)
     cat(
-      paste(
-        "C-score: \n  ",
-        coxend,
-        "  ",
-        f(1),
-        "\n  SRF_Ensemble ",
-        f(2),
-        "\nAUCROC:\n  ",
-        coxend,
-        "  ",
-        f(3),
-        "\n  SRF_Ensemble ",
-        f(4),
-        sep = ""
-      )
-    )
+      paste( "Mean C-score: \n  ",coxend,"  ",
+             f(1),"\n  Ensemble ",f(2),
+             "\nMean AUCROC:\n  ",coxend,"  ",f(3+ii),
+             "\n  Ensemble ",f(4+ii),sep = ""))
     invisible(object)
   }
 
 #' Print survcompare object
-#'
 #' @param x output object of the survcompare function
 #' @param ... additional arguments to be passed
 #' @return x
@@ -108,9 +93,7 @@ print.survcompare <- function(x, ...) {
   if (!inherits(x, "survcompare")) {
     stop("Not a \"survcompare\" object")
   }
-
   summary.survcompare(x)
-
   cat("\n", "See other items as x$item. Items available:\n")
   cat(names(x), sep = ", ")
   invisible(x)
