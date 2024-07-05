b07881d1800f3b0cbd74d73b63383952b33e691c
Diana Shams <diana.shamsutdinova.github@gmail.com>
2024-07-05 15:20:02 +0100

    Update surv_ensemble.R

--- R/surv_ensemble.R
+++ R/surv_ensemble.R
@@ -287,12 +287,15 @@ summary.survensemble_cv <- function(object, ...) {
   if (!inherits(object, "survensemble_cv")) {
     stop("Not a \"survensemble_cv\" object")
   }
-  cat("Cross-validation results\n")
+  cat("Cross-validation results. Computation time:",
+      round(object$time,2), "sec. \n")
 
   if (!is.null(cl <- object$call)) {
     cat("Call:\n")
     dput(cl)
   }
+  print(round(object$testaverage,4))
+
   cat("\nThe stats are computed from the ",
       dim(object$test)[1],
       " data splits.\n")
